447e97ef547a65f8c58ef632366f31df543173b2
zilin xu <zx112@duke.edu>
2024-03-13 14:40:02 -0400

    finish Hello

--- golang/language-learning/code/src/hello/hello.go
+++ golang/language-learning/code/src/hello/hello.go
@@ -3,14 +3,39 @@ package main
 import "fmt"
 
 const englishHello = "Hello, "
+const spanishHello = "Hola, "
+const frenchHello = "Bonjour, "
+const spanish = "Spanish"
 
-func Hello(name string) string {
+// func Hello(name string, language string) string {
+// 	if name == "" {
+// 		name = "World"
+// 	}
+// 	if language == spanish {
+// 		return spanishHello + name
+// 	}
+// 	return englishHello + name
+// }
+
+// 3.13 refactor code
+
+func Hello(name string, language string) string {
 	if name == "" {
-		return englishHello + "World"
+		name = "World"
 	}
-	return englishHello + name
+	return greetingPrefix(language) + name
 }
-
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case "French":
+		prefix = frenchHello
+	case "Spanish":
+		prefix = spanishHello
+	case "English":
+		prefix = englishHello
+	}
+	return
+}
 func main() {
-	fmt.Println(Hello("Bruce"))
+	fmt.Println(Hello("Bruce", "English"))
 }
--- golang/language-learning/code/src/hello/hello_test.go
+++ golang/language-learning/code/src/hello/hello_test.go
@@ -10,16 +10,30 @@ func TestHello(t *testing.T) {
 		}
 	}
 
+	//3.9 add Spanish cases
+	t.Run("this is testing for Spanish hello", func(t *testing.T) {
+		got := Hello("Jose", "Spanish")
+		want := "Hola, Jose"
+		assertCorrectHello(t, got, want)
+	})
+
 	t.Run("this is testing for hello with name", func(t *testing.T) {
-		got := Hello("Bruce")
+		got := Hello("Bruce", "English")
 		want := "Hello, Bruce"
 
 		assertCorrectHello(t, got, want)
 	})
 
 	t.Run("this is testing for hello with empty string", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "English")
 		want := "Hello, World"
+
+		assertCorrectHello(t, got, want)
+	})
+
+	t.Run("this is testing for French hello", func(t *testing.T) {
+		got := Hello("Louis", "French")
+		want := "Bonjour, Louis"
 
 		assertCorrectHello(t, got, want)
 	})
